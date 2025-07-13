6614a4d91b6173a793c63cdee97454a7df4c4c10
Pacio <4995125@163.com>
2025-07-13 22:35:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,10 @@
 # See /LICENSE for more information.
 #
 
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git hworld https://github.com/sbwml/openwrt_helloworld' feeds.conf.default
+sed -i '2i src-git opmos https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
